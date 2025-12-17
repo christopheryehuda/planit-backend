56c08f2330713316b5ecc8500569a661aae5a7c4
christopheryehuda <oakun8751@gmail.com>
2025-12-17 16:20:35 +0700

    fix: handle undefined database url

--- config/db.js
+++ config/db.js
@@ -1,16 +1,41 @@
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 const pg = require('pg'); 
+
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectModule: pg,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
+let db;
+
+if (process.env.DATABASE_URL) {
+    // Jika di Vercel pakai DATABASE_URL
+    db = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        dialectModule: pg,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
         }
-    }
-});
+    });
+} else {
+    // Jika di Vercel pakai variabel eceran (Host, Name, User, Pass)
+    db = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: 'postgres',
+            dialectModule: pg,
+            dialectOptions: {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            }
+        }
+    );
+}
+
 module.exports = db;
