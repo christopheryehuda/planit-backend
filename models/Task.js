eb0c01ae08eab303f4a1c1cf14579978b77d16ef
christopheryehuda <oakun8751@gmail.com>
2025-12-17 23:30:56 +0700

    perubahan task

--- models/Task.js
+++ models/Task.js
@@ -4,31 +4,15 @@ const Users = require('./User');
 
 // PERBAIKAN: Gunakan db.define (bukan dsefine)
 const Tasks = db.define('tasks', {
-    title: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    description: {
-        type: DataTypes.TEXT,
-        allowNull: true
-    },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    deadline: {
-        type: DataTypes.DATE,
-        allowNull: false
-    },
-    priority: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-}, {
-    freezeTableName: true
-});
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: true },
+    category: { type: DataTypes.STRING, allowNull: true }, // Ubah ke true agar tidak error jika kosong
+    deadline: { type: DataTypes.DATE, allowNull: true },    // Ubah ke true
+    priority: { type: DataTypes.STRING, allowNull: true },    // Ubah ke true
+    userId: { type: DataTypes.INTEGER, allowNull: false }
+}, { freezeTableName: true });
 
 Users.hasMany(Tasks);
 Tasks.belongsTo(Users, { foreignKey: 'userId' });
 
-module.exports = Task;
+module.exports = Tasks;
